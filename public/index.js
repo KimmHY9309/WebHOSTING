641cc6cd9a77be4811aa178654260c9f7793f9a0
KimmHY9309 <68420164+KimmHY9309@users.noreply.github.com>
2021-03-17 23:41:16 +0900

    Delete public directory

--- public/index.js
+++
@@ -1,7 +0,0 @@
-const items = document.querySelector('.nav-link');
-//const about = document.querySelector('#carouselExampleCaptions');
-
-items.addEventListener('click', function(e){
-//about.classList.toggle('d-none');
-alert(e.target + '을 클릭했습니다');
-})
